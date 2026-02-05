9319a7ba65741de58568bdbd64b623803cf4546a
Bianca Dollente <biancaadrienne24@gmail.com>
2026-02-06 00:01:48 +0800

    feat: add new class /category

---
+++ Data/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RandomQuoteApi.Data;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public List<Quote> Quotes { get; set; } = new();
+}
