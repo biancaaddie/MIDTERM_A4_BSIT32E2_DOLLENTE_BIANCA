946c1b88905c28c50a942cb63b133bf742ef504e
Bianca Dollente <biancaadrienne24@gmail.com>
2026-02-06 00:02:49 +0800

    feat: add new class/quote

---
+++ Data/Quote.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RandomQuoteApi.Data;
+
+public class Quote
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Text { get; set; } = string.Empty;
+
+    [Required]
+    public string Author { get; set; } = string.Empty;
+    public int CategoryId { get; set; }
+
+    public Category? Category { get; set; }
+}
