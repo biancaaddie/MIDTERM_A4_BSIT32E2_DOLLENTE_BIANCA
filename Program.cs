a6cc5348cedc71023be0566aaa0798764aa0c0a1
Bianca Dollente <biancaadrienne24@gmail.com>
2026-02-06 00:06:09 +0800

    chore: update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using RandomQuoteApi.Data;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+    policy => policy.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+});
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-}
 
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
+app.UseCors("AllowReactApp");
 app.MapControllers();
-
 app.Run();
